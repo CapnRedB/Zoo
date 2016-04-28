46ca7e77880f44b383b6989eb10f531afe10cb86
Aaron Levinson <aaron.levinson1000@gmail.com>
2016-04-28 14:11:07 -0500

    Finished except pending tests

--- javascript/animal.js
+++ javascript/animal.js
@@ -1,10 +1,29 @@
-function Animal(kind){
+function Animal(kind, sound, name, age){
   // write code here
   this.kind = kind;
+  this.sound=sound;
+  this.name=name;
+  this.age=age;
+  this.awake=false;
 }
 
-Animal.prototype  // write more code here
-
+Animal.prototype.wakeUp = function(){
+    this.awake=true;
+} // write more code here
+Animal.prototype.speak = function(){
+  return this.sound;
+}
+Animal.prototype.feed = function(){
+  if(this.awake===true)
+    return "NOM NOM NOM";
+  return undefined;
+}
+Animal.prototype.growUp = function(){
+  this.age++;
+}
+Animal.prototype.sleep = function(){
+  awake=false;
+}
 
 
 module.exports = Animal;
--- javascript/zoo.js
+++ javascript/zoo.js
@@ -1,11 +1,49 @@
 var Animal = require('./animal');
 
-function Zoo() {
+function Zoo(location, name) {
  // write code here
+ this.location=location;
+ this.name=name;
+ this.animals= [];
+ this.status="closed";
 }
 
 
-Zoo.prototype // write more code here
-
-
+Zoo.prototype.isOpen = function(){
+  if(this.status==="closed")
+    return false;
+  if(this.status==="open")
+    return true;
+  return false;
+} // write more code here
+Zoo.prototype.open = function(){
+  this.status="open";
+}
+Zoo.prototype.addAnimal = function(animal){
+  if(animal instanceof Animal){
+  if(this.isOpen()){
+    for(var i=0;i<this.animals.length; i++){
+      if(animal.name===this.animals[i].name){
+        return "Animal is already in.";
+      }
+    }
+    this.animals.push(animal);
+  }
+}
+}
+Zoo.prototype.removeAnimal = function(animal){
+  if(this.isOpen()){
+  for(var i=0; i<this.animals.length; i++){
+    if(animal.name===this.animals[i].name){
+      this.animals.splice(i, 1)
+    }
+  }
+}
+}
+Zoo.prototype.changeLocation = function(location){
+  this.location=location;
+}
+Zoo.prototype.close = function(){
+  this.status="closed";
+}
 module.exports = Zoo;
